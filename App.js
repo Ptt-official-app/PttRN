51fe24872add9344546e112b09f4a6721ce522be
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2021-08-29 01:22:25 -0400

    Initial commit
    Generated by create-react-native-app 3.6.0.

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Router, Route, Link } from './react-router';
+
+const Home = () => <Text>Home</Text>;
+
+const About = () => <Text>About</Text>;
+
+const App = () => (
+  <Router>
+    <View style={styles.container}>
+      <View style={styles.nav}>
+        <Link to="/">
+          <Text>Home</Text>
+        </Link>
+        <Link to="/about">
+          <Text>About</Text>
+        </Link>
+      </View>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </View>
+  </Router>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    padding: 10
+  },
+  nav:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
+
+export default App;
