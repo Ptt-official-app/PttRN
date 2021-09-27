2068e1be2da103aa0597bcc5f669d97ab525f25d
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-09-27 23:57:24 +0800

    Add color for user count icon Use another icon to represent 爆

--- src/component/BoardListItem.tsx
+++ src/component/BoardListItem.tsx
@@ -12,7 +12,9 @@ export default class BoardListItem extends Component<{
             <View style={styles.listItem}>
                 <View style={styles.firstRow}>
                     <Text style={[styles.text, styles.primaryText]}>{this.props.board.brdname}</Text>
-                    <Icon style={{marginRight: 3}} name={'person'} size={24}
+                    <Icon style={{marginRight: 3}}
+                          name={this.props.board.nuser < 100 ? 'person' : 'people'}
+                          size={24}
                           color={this.iconColor(this.props.board.nuser)}/>
                     <Text style={styles.text}>{utils.numberWithCommas(this.props.board.nuser)}</Text>
                 </View>
@@ -22,10 +24,27 @@ export default class BoardListItem extends Component<{
     }
     iconColor(nuser) {
         // TODO
-        if (nuser < 11) {
+        if (nuser <= 10) {
             return '#CCC'
+        } else if (nuser <= 50) {
+            return '#CC0'
+        } else if (nuser <= 99) {
+            return '#D32'
+        } else if (nuser <= 999) {
+            return '#FFF'
+        } else if (nuser <= 4999) {
+            return '#F00'
+        } else if (nuser <= 9999) {
+            return '#00F'
+        } else if (nuser <= 29999) {
+            return '#0DF'
+        } else if (nuser <= 59999) {
+            return '#0F0'
+        } else if (nuser <= 99999) {
+            return '#FF0'
+        } else {
+            return '#F0F'
         }
-        return '#F00'
     }
 }
 
